dfecc185a845db45fcfdc97a38b43c454966278e
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-02 00:39:05 -0600

    $git commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { Productos } from './components/products';
 import { Details } from './components/Details';
 import { Cart } from './components/Cart';
 import { blogs } from './data/dataeng';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+
 import Swal from 'sweetalert2';
 import { useState } from 'react';
 
