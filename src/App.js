97ebaae52d14b53d74777c5c9cb21715bd39ead6
Leyla Carmona <leylacarmona2010@hotmail.es>
2024-11-02 21:13:54 -0600

    $git commit

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ function App() {
               <li><Link to="/home">Home</Link></li>
               <li><Link to="/products">Our products</Link></li>
               <li><Link to="/cart">Cart</Link></li>              
-            </ul>
+              <li><a href="https://leyla-carmona.github.io/Portfolio/index.html">Portfolio</a></li>    
+              </ul>
           </nav>
         </header>
         <div>
